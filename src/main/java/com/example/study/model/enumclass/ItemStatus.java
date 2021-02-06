68444fc0aa7d1bbdbe559ebf99a166b07e68d4e1
tjdehd3698 <khdkhd12@gmail.com>
2021-02-06 13:38:25 +0900

    emum 적용

---
+++ src/main/java/com/example/study/model/enumclass/ItemStatus.java
@@ -0,0 +1,18 @@
+package com.example.study.model.enumclass;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ItemStatus {
+
+    REGISTERED(0, "등록상태","사용자 등록상태"),
+    UNREGISTERED(1,"해지", "사용자 해지상태"),
+    WAITING(2, "검수(대기)", "상품 검수 상태")
+    ;
+
+    private Integer id;
+    private  String title;
+    private  String description;
+}
