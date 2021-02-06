616d39ba1057b0f675d00cf965d789f1a13480f9
tjdehd3698 <khdkhd12@gmail.com>
2021-02-06 14:45:15 +0900

    추상화 코드 적용

---
+++ src/main/java/com/example/study/service/PartnerApiLogicService.java
@@ -0,0 +1,54 @@
+package com.example.study.service;
+
+import com.example.study.model.entity.OrderGroup;
+import com.example.study.model.entity.Partner;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.PartnerApiRequest;
+import com.example.study.model.network.response.OrderGroupApiResponse;
+import com.example.study.model.network.response.PartnerApiResponse;
+
+public class PartnerApiLogicService extends BaseService<PartnerApiRequest, PartnerApiResponse, Partner> {
+
+    @Override
+    public Header<PartnerApiResponse> create(Header<PartnerApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header<PartnerApiResponse> read(Long id) {
+
+        baseRepository.findById(id)
+                .map(partner->response(partner))
+                .orElseGet(()->Header.ERROR("데이터 없음"));
+        return null;
+    }
+
+    @Override
+    public Header<PartnerApiResponse> update(Header<PartnerApiRequest> request) {
+        return null;
+    }
+
+    @Override
+    public Header<PartnerApiResponse> delete(Long id) {
+        return null;
+    }
+
+    private Header<PartnerApiResponse> response(Partner partner){
+
+        PartnerApiResponse body = PartnerApiResponse.builder()
+                .id(partner.getId())
+                .name(partner.getName())
+                .status(partner.getStatus())
+                .address(partner.getAddress())
+                .callCenter(partner.getCallCenter())
+                .partnerNumber(partner.getPartnerNumber())
+                .businessNumber(partner.getBusinessNumber())
+                .ceoName(partner.getCeoName())
+                .registeredAt(partner.getRegisteredAt())
+                .unregisteredAt(partner.getUnregisteredAt())
+                .categoryId(partner.getCategory().getId())
+                .build();
+
+        return Header.Ok(body);
+    }
+}
